59aaed4adb26ec18a57f39033c178cfec91bc2be
Jose Cabrera <jcabrera@estratek.com>
2019-08-14 00:51:24 -0600

    siteconfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,9 +38,9 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
+    {blog: true, label: 'Blog'},
     {doc: 'doc1', label: 'Articulos'},
     {doc: 'doc4', label: 'Eventos'},
-    {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
